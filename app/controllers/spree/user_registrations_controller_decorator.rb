d90447c3cc077333024c2c7d81741af25ca3e360
Noel <noel@2bedigital.com>
2019-02-20 12:07:11 +0000

    bug in user registration controller

--- app/controllers/spree/user_registrations_controller_decorator.rb
+++ app/controllers/spree/user_registrations_controller_decorator.rb
@@ -3,7 +3,7 @@ Spree::UserRegistrationsController.class_eval do
   after_action :segment_complete_registration, only: :create, if: -> { flash[:notice] }
 
   def segment_complete_registration
-    Track::User.new(@user, request).logged_in_event('CompleteRegistration',
+    Track::User.new(spree_current_user, request).logged_in_event('CompleteRegistration',
                                                     {currency: current_currency,
                                                      value: current_order(create_order_if_necessary: true).total,
                                                      content_name: request.path
--- lib/solidus_segment/version.rb
+++ lib/solidus_segment/version.rb
@@ -1,3 +1,3 @@
 module SolidusSegment
-  VERSION = '1.0.3'
+  VERSION = '1.0.4'
 end
